03a48b26912eac93ed2740e1595784ccd3cfc3e0
Ana Julia <julia.sousa@aluno.ifsp.edu.br>
2025-09-09 12:03:41 -0300

    atividade_18

---
+++ entregas/atividade_18/progress_bar.gd
@@ -0,0 +1,10 @@
+extends Control
+func _on_timer_timeout() -> void:
+	$TextureProgressBar.value +=1
+func _on_button_pressed() -> void:
+	$TextureProgressBar.value +=10
+func _on_texture_progress_bar_value_changed(value: float) -> void:
+	if value >= $TextureProgressBar.max_value:
+		$TextureProgressBar.max_value = $TextureProgressBar.max_value * 1.2
+		$TextureProgressBar/Label.text =str(int($TextureProgressBar/Label.text) + 1)
+		$TextureProgressBar.value = 0
