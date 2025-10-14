f0ba44647f84e854dca37975d27a253993798939
Ana Julia <julia.sousa@aluno.ifsp.edu.br>
2025-10-14 12:06:43 -0300

    particulas

--- entregas/atividade_18/progress_bar.gd
+++ entregas/atividade_18/progress_bar.gd
@@ -8,3 +8,4 @@ func _on_texture_progress_bar_value_changed(value: float) -> void:
 		$TextureProgressBar.max_value = $TextureProgressBar.max_value * 1.2
 		$TextureProgressBar/Label.text =str(int($TextureProgressBar/Label.text) + 1)
 		$TextureProgressBar.value = 0
+		$CPUParticles2D.emitting = true
