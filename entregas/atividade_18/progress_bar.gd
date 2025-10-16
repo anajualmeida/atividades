2a56d4e5f1462d752a132b624d53060eaf15fe81
anajualmeida <julia.sousa@aluno.ifsp.edu.br>
2025-10-16 16:24:30 -0300

    atividade_22

--- entregas/atividade_18/progress_bar.gd
+++ entregas/atividade_18/progress_bar.gd
@@ -1,11 +1,13 @@
 extends Control
 func _on_timer_timeout() -> void:
 	$TextureProgressBar.value +=1
+
 func _on_button_pressed() -> void:
-	$TextureProgressBar.value +=10
+	$TextureProgressBar.value +=50
+
 func _on_texture_progress_bar_value_changed(value: float) -> void:
 	if value >= $TextureProgressBar.max_value:
-		$TextureProgressBar.max_value = $TextureProgressBar.max_value * 1.2
+		$TextureProgressBar.max_value = $TextureProgressBar.max_value * 1.1
 		$TextureProgressBar/Label.text =str(int($TextureProgressBar/Label.text) + 1)
 		$TextureProgressBar.value = 0
 		$CPUParticles2D.emitting = true
